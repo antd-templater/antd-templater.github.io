10bace3bf81353929e20fc095bcd4cbf48db0179
linpengteng <china.linpengteng@gmail.com>
2022-10-05 11:28:38 +0800

    fix(views): css-layout

--- vue2.x/js/vendors.js
+++ vue2.x/js/vendors.js
@@ -77257,7 +77257,7 @@ module.exports = {
         var Promise = promiseForIE || win().Promise
         var _this = this;
         if (!theme_COLOR_config) {
-            theme_COLOR_config = win()["tc_cfg_2913663157378037"]
+            theme_COLOR_config = win()["tc_cfg_9263048158442231"]
             var later = retry()
             //重试直到theme_COLOR_config加载
             if (later) return later
