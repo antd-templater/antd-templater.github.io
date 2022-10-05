8cea63f9b454a148f0ddafb053656ccc979a53ac
linpengteng <china.linpengteng@gmail.com>
2022-10-05 16:01:49 +0800

    fix(components): BaseTree

--- vue2.x/js/vendors.js
+++ vue2.x/js/vendors.js
@@ -77257,7 +77257,7 @@ module.exports = {
         var Promise = promiseForIE || win().Promise
         var _this = this;
         if (!theme_COLOR_config) {
-            theme_COLOR_config = win()["tc_cfg_9530734431426471"]
+            theme_COLOR_config = win()["tc_cfg_07836551805649017"]
             var later = retry()
             //重试直到theme_COLOR_config加载
             if (later) return later
